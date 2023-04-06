ccb40622c0bc23e1b379d33645adc3ab41fc31ac
osgrass <64144934+osgrass@users.noreply.github.com>
2023-04-06 11:14:26 -0400

    Update menu.py

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -2,6 +2,8 @@ from ssg import hooks, parsers
 
 files = []
 
+
+@hooks.register("collect_files")
 def collect_files(source, site_parsers):
     valid = lambda p : not isinstance(p, parsers.ResourceParser)
     for path in source.rglob("*"):
