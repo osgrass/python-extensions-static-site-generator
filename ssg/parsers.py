e7422c617074bad37ba427c0013af4d81e644366
osgrass <64144934+osgrass@users.noreply.github.com>
2023-04-06 11:41:03 -0400

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -64,3 +64,4 @@ class ReStructuredTextParser(Parser):
         sys.stdout.write(
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
         )
+        hooks.event("written")
